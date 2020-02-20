19395abb30c339b85ea3666c48dd0d6db5116f3f
Visal Rajapakse <visalrajapakse@gmail.com>
2020-02-20 10:15:17 +0530

    almost completed on tutorial 3.1

---
+++ week 3/ActivityIntents/app/src/main/java/com/visal/activityintents/MainActivity.java
@@ -0,0 +1,32 @@
+package com.visal.activityintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
+    private EditText mMessageEditText;
+
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMessageEditText = (EditText) findViewById(R.id.editText_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Main Button pressed");
+        String mainMessage = mMessageEditText.getText().toString();
+        startActivity(new Intent(MainActivity.this, SecondActivity.class).putExtra(EXTRA_MESSAGE, mainMessage));
+
+    }
+}
